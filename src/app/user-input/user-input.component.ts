8a3d5a96e6f33f6a12b11af2b96cdda336a9b401
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2024-07-11 11:19:32 +0530

    Usage of service

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,20 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  enteredInitialInvestment='0';
-  enteredAnnualInvestment='0';
-  enteredExpectedReturn='5';
-  enteredDuration='10';
-  onSubmit(){
-    console.log(this.enteredAnnualInvestment);
-    
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+
+  constructor(private investmentService: InvestmentService) {}
+
+  onSubmit() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      duration: +this.enteredDuration(),
+    });
+
+    this.enteredAnnualInvestment.set('0');
+    this.enteredDuration.set('10');
+    this.enteredExpectedReturn.set('5');
+    this.enteredInitialInvestment.set('0');
   }
 }
